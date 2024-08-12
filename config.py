916dd09eeb1922d08666a5cdaecc1c7d4c6d1aed
David Kerr <kerrky@gmail.com>
2024-08-12 13:54:03 -0400

    Updated enviroment virable

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from app import app
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,5 +8,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
