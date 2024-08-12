233a93f1d2603a515459d14733bfebbc13a514d4
David Kerr <kerrky@gmail.com>
2024-08-12 13:43:01 -0400

    Updated enviroment virable

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
